120378d2ba89e2cda51b46efa01d6a5e5e3db8f4
Quan Tran (QuanTNH) <quantnh@mti-tech.vn>
2023-08-27 19:24:10 +0700

    update code

--- central_server.py
+++ central_server.py
@@ -1,22 +1,56 @@
 import flwr as fl
+from flwr.common import Metrics
 from flwr.server.client_manager import SimpleClientManager
 from flwr.server.strategy import FedAvg
+from flwr.server import ServerConfig
+
+
+def weighted_average(metrics: list[tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+def fit_config(server_round: int):
+    """Return training configuration dict for each round.
+
+    Perform two rounds of training with one local epoch, increase to two local
+    epochs afterwards.
+    """
+    config = {
+        "server_round": server_round,  # The current round of federated learning
+        "local_epochs": 1 if server_round < 2 else 2,  #
+    }
+    return config
 
 
 def main():
-
     client_manager = SimpleClientManager()
-    strategy = FedAvg()
+    # client_manager = (SimpleClientManager([client]),)
+    print("Number of clients: ", len(client_manager))
+    strategy = FedAvg(
+        # fraction_fit=1.0,
+        # fraction_evaluate=0.5,
+        # min_fit_clients=10,
+        # min_evaluate_clients=5,
+        # min_available_clients=10,
+        evaluate_metrics_aggregation_fn=weighted_average,  # Pass the metric aggregation function
+        # initial_parameters=fl.common.ndarrays_to_parameters(params),
+        on_fit_config_fn=fit_config,  # Pass the fit_config function to the server
+    )
 
-    # Start Flower server
+    # Start the Flower server and connect the client
+    server_config = ServerConfig(num_rounds=10)
     fl.server.start_server(
         server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(
-            num_rounds=3,
-        ),
+        config=server_config,
         client_manager=client_manager,
-        strategy=strategy
+        strategy=strategy,
     )
+    print("Number of clients: ", len(client_manager))
 
 
 if __name__ == "__main__":
--- client_device.py
+++ client_device.py
@@ -1,33 +1,73 @@
 import os
-
 import flwr as fl
 import tensorflow as tf
-
 
 # Make TensorFlow log less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-# Load model and data (MobileNetV2, CIFAR-10)
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-
-# Define Flower client
+# Define the Flower client
 class CifarClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
-        return model.get_weights()
+    def __init__(self, model, xy_train, xy_test):
+        self.model = model
+        self.xy_train = xy_train
+        self.xy_test = xy_test
+
+    def get_parameters(self, config):  # override
+        print("[Client] get_parameters")
+        return self.model.get_weights()
+
+    def set_parameters(self, parameters):
+        self.model.set_weights(parameters)
 
     def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32)
-        return model.get_weights(), len(x_train), {}
+        # Read values from config
+        server_round = config["server_round"]
+        local_epochs = config["local_epochs"]
+
+        # Use values provided by the config
+        print(f"[Client, round {server_round}] fit, config: {config}")
+        self.model.set_weights(parameters)
+        self.model.fit(
+            self.xy_train[0],
+            self.xy_train[1],
+            epochs=local_epochs,
+            batch_size=32,
+            verbose=0,
+        )
+        # NOTE: return parameters, num_examples, metrics
+        return self.model.get_weights(), len(self.xy_train[0]), {}
 
     def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": accuracy}
+        print(f"[Client] evaluate, config: {config}")
+        self.model.set_weights(parameters)
+        loss, accuracy = self.model.evaluate(self.xy_test[0], self.xy_test[1])
+        return float(loss), len(self.xy_test[0]), {"accuracy": accuracy}
 
 
-# Start Flower client
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=CifarClient())
+def main():
+    # Load model and data (MobileNetV2, CIFAR-10)
+    model = tf.keras.applications.MobileNetV2(
+        (32, 32, 3), classes=10, weights=None
+    )
+    model.compile(
+        "adam", "sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    # Load and preprocess your dataset (for illustration purposes, we assume you have loaded CIFAR-10 data)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    # Create the Flower client
+    client = CifarClient(
+        model=model,
+        xy_train=(x_train, y_train),
+        xy_test=(x_test, y_test),
+    )
+
+    # Start the training process
+    fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=client)
+
+
+if __name__ == "__main__":
+    main()
